d0f981465e2faa978ff080bc8efc468fe5563515
lugoDv <falugopersonal@gmail.com>
2025-02-24 21:49:11 +0100

    finished part 1.6

--- part_1/Unicafe/src/App.jsx
+++ part_1/Unicafe/src/App.jsx
@@ -1,15 +1,50 @@
 import { useState } from 'react'
 
 
+const Button = ({handleClick,nameButton}) => {
+  
+  
+
+
+  return(
+
+    <button onClick={handleClick}>{nameButton}</button>
+  )
+
+}
+
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const setToGood = () => {
+    console.log(good,"aumenta un good")
+    setGood(good + 1)
+  }
+
+  const setToneutral = () =>{
+    console.log(neutral,'aumenta neutral')
+    setNeutral(neutral+ 1 )
+  }
+
+  const setToBad = () => {
+    console.log(bad,"aumenta bad")
+    setBad(bad + 1)
+  }
+
   return (
     <div>
-      code here
+      <h2>give feedback</h2>
+      
+      <Button handleClick = {() => setToGood()} nameButton="good"/>
+      <Button handleClick={()=> setToneutral()} nameButton='neutral'></Button>
+      <Button handleClick={() => setToBad()} nameButton="bad"/> 
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
